a73e8974377b8e50da6ba0fdc1cd17860432e333
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-11-03 02:32:52 +0600

    reuse component done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,40 +1,39 @@
 <template>
 <div>
-    <h1>Pass data from parent to child component</h1>
-    <Child name="Data passed from parent to child component" :user="user" :getData="getData"/>  
+    <h1>Reuse Componet</h1>
+    <ul>
+        <li v-for="item in users" :key="item.name">
+            <User :data="item" :getData="getData"/>
+        </li>
+    </ul>
 </div>
 </template>
 
 <script>
-import Child from "./Child.vue"
+import User from "./User.vue"
 export default {
     name: "Home",
     components: {
-        Child
+        User
     },
-    data() {
-        return {            
-            user: [
-                {
-                    name: "Shubrato",
-                    email: "shubrato@gmail.com"
-                },
-                {
-                    name: "Shuvo",
-                    email: "shuvo@gmail.com"
-                },
-                {
-                    name: "Shovan",
-                    email: "shovan@gmail.com"
-                },
+    data(){
+        return{
+            users:[
+                { name: "Shubrato",email: "shubrato@gmail.com"},
+                { name: "Shuvo",email: "shuvo@gmail.com"},
+                { name: "Shovan",email: "shovan@gmail.com"},
+                { name: "Tonmoy",email: "tonmoy@gmail.com"},
+                { name: "JH",email: "jh@gmail.com"},
+                { name: "SK",email: "sk@gmail.com"},
             ]
         }
     },
-    methods: {
-        getData(){
-            alert("Function called at child component which passed from parent component.")
+    methods:{
+        getData(name){
+            alert(`Function called by ${name}`)
         }
     }
+    
 }
 </script>
 
---
+++ src/components/User.vue
@@ -0,0 +1,24 @@
+<template >
+    <div class="user">
+        <h2>Name: {{data.name}}</h2>
+        <h3>Email: {{data.email}}</h3>
+        <button v-on:click="getData(data.name)">Alert Message</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "User",
+    props:{
+        data: Object,
+        getData: Function
+    }
+}
+</script>
+<style scoped>
+    .user{
+        background-color: skyblue;
+        padding: 10px;
+        border-bottom: 1px solid orangered;
+        margin-bottom: 20px;
+    }
+</style>
