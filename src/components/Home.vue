f60041ab8ba681530b4fe21b2ea0ecb18bbbccff
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-11-02 23:59:12 +0600

    if else condition done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,9 @@
 <template>
 <div>
-    <h1>Get Checkbox & Radio Button Values</h1>
-    <h3>Technologies</h3>
-    <label for="javascript">JavaScript</label>
-    <input type="checkbox" value="JavaScript" v-model="technology" id="javascript">
-    <br>
-    <label for="php">PHP</label>
-    <input type="checkbox" value="PHP" v-model="technology" id="php">
-    <br>
-    <label for="swift">Swift</label>
-    <input type="checkbox" value="Swift" v-model="technology" id="swift">
-    <br>
-    <label for="rust">Rust</label>
-    <input type="checkbox" value="Rust" v-model="technology" id="rust">
-    <br>
-    <label for="node">Node</label>
-    <input type="checkbox" value="Node" v-model="technology" id="node">
-    <br>
-    <br>
-    <h3>Who I am </h3>
-    <label for="student">Student</label>
-    <input type="radio" value="Student" v-model="who" name="I" id="student">
-    <br>
-    <label for="developer">Frontend Developer</label>
-    <input type="radio" value="Frontend Developer" v-model="who" name="I" id="developer">
-    <br>
-    <label for="developer1">Full Stack Developer</label>
-    <input type="radio" value="Full Stack Developer" v-model="who" name="I" id="developer1">
-    <br>
-    <br>
-    <h4>Selected Technologies: {{technology}}</h4>
-    <br>
-    <h4>I'm a {{who}}</h4>
+    <h1>If-else conditions in Vue JS</h1>
+    <h2 v-if="show">If-else condition</h2>
+    <h2 v-else>Else Condition</h2>
+    <button v-on:click="show">If-else toggle</button>
 
 </div>
 </template>
@@ -41,8 +13,7 @@ export default {
     name: "Home",
     data() {
         return {
-            technology: [],
-            who: null
+            show: false,
         }
     }
 }
