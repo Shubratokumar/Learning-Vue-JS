a7e0ddb8f54456bef4638f74ab4dc030a4e973ff
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-11-03 03:23:44 +0600

    Class binding done with dynamic class

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,8 @@
 <template>
 <div>
     <h1>Class Binding</h1>
-    <h3 :class="{purple:colorfull}">This is Home</h3>
+    <h3 :class="applyStyle">This is Home</h3>
     <button v-on:click="colorfull=!colorfull">{{colorfull ? "Remove style" : "Apply style"}}</button>
-
 
 </div>
 </template>
@@ -11,12 +10,21 @@
 <script>
 export default {
     name: "Home",
-    data(){
-        return{
-            colorfull: !true,
+    data() {
+        return {
+            colorfull: false,
         }
     },
-    
+    computed: {
+        applyStyle() {
+            return {
+                purple: this.colorfull,
+                err: true,
+                other: true
+            }
+        }
+    }
+
 }
 </script>
 
@@ -24,10 +32,19 @@ export default {
 h1 {
     color: orange;
 }
-.purple{
+
+.purple {
     background-color: rebeccapurple;
     color: chartreuse;
-    width: 200px;
+    width: 250px;
     padding: 10px;
+}
+
+.other {
+    font-size: 34px;
+}
+
+.err {
+    color: red
 }
 </style>
