bd09fe196b6b5b2c73f9ab19da0f41c62709056d
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-11-03 17:00:10 +0600

    non props data done

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,20 +1,24 @@
 <template lang="">
-        <h4 v-bind="$attrs">Peter</h4>
+    <div >
+        <h4 >Peter</h4>
         <h4>Bruch</h4>
         <h4>Anil</h4>
         <h4>Shidhu</h4>
-        <h4>{{data}}</h4>
+        <h4 v-bind="$attrs">{{data}}</h4>
+    </div>
 </template>
 <script>
 export default {
     name: "User",
+    // inheritAttrs: false,
     data(){
         return{
             userName: "Shubrato"
         }
     },
     props: {
-        data: String
+        data: String,
+        id: String
     }
     
 }
