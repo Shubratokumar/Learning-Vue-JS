9fa6779083374dc91819870f75ab56953b63ae8d
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-11-03 14:17:33 +0600

    Complete form done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,34 @@
 <template>
 <div>
     <h1>Simple Form</h1>
+    <p>{{form}}</p>
     <form action="">
         <label for="">Email :</label>
         <input type="text" placeholder="Enter Email" v-model="form.email">
         <br>
         <label for="">Password :</label>
         <input type="password" placeholder="Enter Password" v-model="form.password">
+        <br>
+        <label for="">Country :</label>
+        <select  v-model="form.country">
+            <option >India</option>
+            <option >Bangladesh</option>
+            <option >USA</option>
+        </select>
+        <br>
+        <h4>Technology: </h4>
+        <label for="">Java :</label>
+        <input type="checkbox" value="Java" v-model="form.technology">
+        <br>
+        <label for="">JavaScript :</label>
+        <input type="checkbox" value="JavaScript" v-model="form.technology">
+        <br>
+        <h4>Gender: </h4>
+        <label for="">Male :</label>
+        <input type="radio" value="Male" name="gender" v-model="form.gender">
+        <br>
+        <label for="">Female :</label>
+        <input type="radio" value="Female" name="gender" v-model="form.gender">
         <br>
         <button v-on:click="login">Login</button>
     </form>
@@ -21,7 +43,10 @@ export default {
         return{
             form: {
                 email: "",
-                password: ""
+                password: "",
+                country: "",
+                technology: [],
+                gender: ""
             }
         }
     },
